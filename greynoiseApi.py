7c6e54bb85cc2533365d1d91dc9d1c424ac12134
joshc1224 <joshua.coe11@yahoo.com>
2019-07-01 16:12:02 +0000

    final greynoiseapi

---
+++ greynoiseApi.py
@@ -0,0 +1,29 @@
+import requests
+import csv
+
+out = 'output.csv'
+f = open(out, 'w')
+with f:
+ fnames = ['source','category','activity','confidence','last_updated','name','intention','first_seen','metadata','tor','rdns_parent','link','org','os','asn','rdns']
+ writer = csv.DictWriter(f, fieldnames=fnames)
+ writer.writeheader()
+ with open('output.csv') as output:
+  with open('input.csv') as csvfile:
+   filereader = csv.reader(csvfile)
+   for row in filereader:
+    for item in row: #item is record
+     print item
+     data = {
+     'ip': item
+     }
+     response = requests.post('http://api.greynoise.io:8888/v1/query/ip', data=data)
+     serials =  response.json()
+     try:
+      y=0
+      for x in range(0,499):
+       serials['records'][x]['source'] = item
+       writer.writerow(serials['records'][x])
+       y+=1
+     except:
+      neg = {'source': item, 'category': str(y) + ' Hits'}
+      writer.writerow(neg)
