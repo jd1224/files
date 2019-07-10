dd1abbf68458c79b0170b8c8687f3a2b9a93c423
Josh C <Joshua.coe11@yahoo.com>
2019-07-10 07:56:28 -0400

    Add files via upload

--- api.py
+++ api.py
@@ -1,15 +1,18 @@
 import requests
 import csv
 import os
+import time 
+
+count = 0
 
 origdir = os.getcwd()
 with open('input.csv') as csvfile:
    filereader = csv.reader(csvfile, delimiter = ',')
    for row in filereader:
     for item in row: #item is record
-     
+    
         ip = item
-
+        print (ip)
         try:
             os.mkdir(ip)
         except Exception as e:
@@ -105,38 +108,5 @@ with open('input.csv') as csvfile:
                     except Exception as e:
                         pass
         os.chdir(origdir)
-        '''
-        out = 'output.csv'
-        f = open(out, 'w')
-        with f:
-            
-            ip = '80.80.80.80'
-            url = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
-            params = {'apikey':'2fb1db9bbb2b4b5b400e54e7375415192664475038c5bd8c9dfcd7693d277339','ip':ip}
-            response = requests.get(url, params=params)
-            serials = response.json()
-            fnames = ['response_code','verbose_msg','asn','country','resolutions','undetected_urls','undetected_referrer_samples','whois','whois_timestamp','detected_urls','undetected_downloaded_samples','detected_communicating_samples','undetected_communicating_samples','network','detected_downloaded_samples','as_owner','detected_referrer_samples','continent']
-            writer = csv.DictWriter(f, fieldnames=fnames)
-            writer.writeheader()
-            
-            print (response)
-            print (serials['resolutions'][0])
-            
-            print (serials['continent'])
-            print (len(serials['detected_referrer_samples']))
-            print (serials['as_owner'])
-            print (len(serials['detected_downloaded_samples']))
-            print (serials['network'])
-            print (len(serials['undetected_communicating_samples']))
-            print (len(serials['detected_communicating_samples']))
-            print (len(serials['undetected_downloaded_samples']))
-            print (len(serials['detected_urls']))
-            print (serials['whois_timestamp'])
-            print (serials['whois'])
-            print (len(serials['undetected_referrer_samples']))
-            print (len(serials['undetected_urls']))
-            print (len(serials['resolutions']))
-            
-            with open('output.csv') as output:
-                writer.writerow(serials)
-        '''
+    time.sleep(16)
+		
